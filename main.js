520f3232b28b8dace0063e24f69e87c28cc2c8ea
KernelOS <romiluna04@gmail.com>
2024-10-08 13:04:40 -0300

    comienzo header y config de api

---
+++ main.js
@@ -0,0 +1,6 @@
+// Configuracion de la API
+const apiUrl = "https://gateway.marvel.com/v1/public/";
+const publicKey = "10f5836a64fa9ea27bf6fb9cbf41e826";
+const privateKey = "c3859b369c4d5d8bb057b8d15b2058ec31f0db25";
+const ts = "marvelromilu";
+const hash = "3625cd0f9789029f5d8d27b459b86464";
